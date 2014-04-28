aa5d93c02d2aee625991c63351cb9d4cde8d644d
Hack Reactor Students <rajatkhanna82@gmail.com>
2014-04-28 16:19:09 -0700

    parsing and storing fetched messages

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -20,19 +20,20 @@ app.send = function(message){
 
 app.init = function(){
 
-
 };
 
 app.fetch =  function(){
+  that = this;
 $.ajax({
     // always use this url
     url: 'https://api.parse.com/1/classes/chatterbox',
     type: 'GET',
-    //data: JSON.stringify(message),
+    order: "-createAt",
     contentType: 'application/json',
     success: function (data) {
       console.log('chatterbox: Message received');
       console.log(data);
+      that.updateData(data.results);
     },
     error: function (data) {
       // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
@@ -41,7 +42,9 @@ $.ajax({
   });
 };
 
-app.messages = {};
+app.messageIDs  = {};
+app.rooms       = {};
+app.users       = {};
 
 
 app.renderMessage = function(message){
@@ -55,10 +58,30 @@ app.renderMessage = function(message){
   var $roomname = $(document.createElement("div")).text(message.roomname);
 
   $msg.append($username).append($createAt).append($text).append($roomname);
-  $("body").append($msg).show();
-
   return $msg;
 };
+
+
+app.updateData = function(messageArray){
+  _.each(messageArray, function(message, i){
+    console.log(message);
+    var id = message.objectId;
+    if(!app.messageIDs[id]){
+      app.messageIDs[id] = true;
+      if(!app.rooms[message.roomname]){
+        app.rooms[message.roomname] = [];
+      }
+      app.rooms[message.roomname].push(message);
+
+      if(!app.users[message.username]){
+        app.users[message.username] = [];
+      }
+
+      app.users[message.username].push(message);
+    }
+  });
+};
+
 
 app.testMessage = {
   'username': 'shawndrost',
